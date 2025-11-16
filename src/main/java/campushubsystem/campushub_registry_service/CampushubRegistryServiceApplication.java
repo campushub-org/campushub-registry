7ccb04bc47d8d6e5b641433af1755433463b505b
Dybray <ngogangbrayan18@gmail.com>
2025-11-16 14:24:56 +0100

    service d'enregistrement

---
+++ src/main/java/campushubsystem/campushub_registry_service/CampushubRegistryServiceApplication.java
@@ -0,0 +1,14 @@
+package campushubsystem.campushub_registry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class CampushubRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampushubRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/campushubsystem/campushub_registry_service/CampushubRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package campushubsystem.campushub_registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampushubRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
